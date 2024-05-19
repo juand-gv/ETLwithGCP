f0d6460820db7dc4bce87f1b32ccc366b2f0a4c4
Juandi <juan_gallegos.tekton@skyairline.com>
2024-05-19 18:32:42 -0500

    Fix. 'restore' cloud function latest blob

--- restore/main.py
+++ restore/main.py
@@ -1,9 +1,12 @@
 from google.cloud import bigquery
 from google.cloud import storage
 from google.cloud import secretmanager
+import pytz  # Se necesita para manejar la zona horaria
+
 import os
 import logging
 from datetime import datetime
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -33,7 +36,7 @@ def find_latest_backup(bucket_name, prefix):
     bucket = storage_client.bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=prefix)
     latest_blob = None
-    latest_date = datetime.min
+    latest_date = datetime.min.replace(tzinfo=pytz.UTC)  # Asegúrate de que 'latest_date' es aware
 
     for blob in blobs:
         blob_date = blob.time_created
@@ -45,29 +48,26 @@ def find_latest_backup(bucket_name, prefix):
 
 def restore_table_from_avro(request):
     """
-    Automatically restores a BigQuery table from the latest AVRO file located in a GCS bucket.
+    Imports a BigQuery table from the latest AVRO file located in a GCS bucket.
     """
     client = bigquery.Client()
-    backups_path = get_secret("bq_backups_file_path").replace('gs://', '')
+    backups_path = get_secret("bq_backups_file_path").strip('/')
     dataset_id = get_secret("mig-dataset-id")
     project_id = __project_id__
 
     payload = request.get_json()
     table_id = payload["table_id"]
-    
-    # Split to get bucket name and base path
-    parts = backups_path.split('/', 1)
-    bucket_name = parts[0]
-    base_path = parts[1] if len(parts) > 1 else ''
 
-    prefix = f"{base_path}{table_id}/"
+    # Removing 'gs://' from backups_path to use only the bucket name and path
+    bucket_name, prefix = backups_path[5:].split('/', 1)
+    prefix = f"{prefix}{table_id}/"
     backup_file_name = find_latest_backup(bucket_name, prefix)
 
     if not backup_file_name:
         logger.error(f"No backup found for {table_id}")
         return f"No backup found for {table_id}", 404
 
-    source_uri = f"gs://{bucket_name}/{backup_file_name}"
+    source_uri = f"gs://{bucket_name}/{prefix}{backup_file_name}"
     dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
     table_ref = dataset_ref.table(table_id)
     job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.AVRO)
